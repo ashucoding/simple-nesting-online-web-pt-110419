18ac693253b19816d15eaec3862d6223aa130d0d
Shana Wiltshire <shanawiltshire@gmail.com>
2019-11-19 03:11:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,8 +16,9 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,15 +57,18 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+# add the following information to the top level of 
 
+programmer_hash =
+[:yukihiro_matsumoto] => {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+ }
+end
 	programmer_hash = 
  		{
         :grace_hopper => {
